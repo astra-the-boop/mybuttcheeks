7516f1ed10369b00e87339cbad11aa1949d140c2
boopIsDying <109295196+boopIsDying@users.noreply.github.com>
2024-07-03 02:38:50 +0700

    added back button and GLOBAL VARSgit add --all

---
+++ scripts/Global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var roomScene_firstLoad = true
+
+var pcScene_pcTurnedOn = false
--- scripts/pcScene.gd
+++ scripts/pcScene.gd
@@ -2,16 +2,35 @@ extends Node2D
 
 @onready var PcScreen = $PcScreen
 @onready var PowerButton = $PowerButton
+@onready var back = $GUI/returnToBed
+
+
+## GUI
+func backButton_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/room.tscn")
 
 
 func powerButton_press() -> void:
 	PcScreen.set_texture(preload("res://assets/PcScene_placeholderOnScreen.png"))
 	PowerButton.set_visible(false)
+	Global.pcScene_pcTurnedOn = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	## PRELOAD
+	if !Global.pcScene_pcTurnedOn:
+		PcScreen.set_texture(preload("res://assets/pcScene_blackScreen.png"))
+	else:
+		PcScreen.set_texture(preload("res://assets/PcScene_placeholderOnScreen.png"))
+		PowerButton.set_visible(false)
+	
+	
+	## ----
 	PowerButton.button_down.connect(powerButton_press)
+	back.button_down.connect(backButton_pressed)
+	
+	
 	
 
 
