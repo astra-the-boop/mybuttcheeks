f548d5dea3ec3ef13dec470bd3867f1ebfe5eae8
boopIsDying <109295196+boopIsDying@users.noreply.github.com>
2024-08-26 13:12:35 +0700

    astp-1.0.1

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -10,9 +10,20 @@ func play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/room.tscn")
 	#in future, set change_scene_to_file based on locaiton based on time/story
 
+func credits_pressed() -> void:
+	OS.shell_open("https://github.com/astra-the-boop/mybuttcheeks/wiki/Credits")
+
+func quit_pressed() -> void:
+	#are you sure you wanna quit? quitting means that you have ZERO style
+	#(class of 09 ref :3)
+	get_tree().quit()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	play.button_down.connect(play_pressed)
+	quit.button_down.connect(quit_pressed)
+	credits.button_down.connect(credits_pressed)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
